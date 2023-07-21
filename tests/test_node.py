143d346b8083f5267e5eb88a7529b56da268a12f
amedeo <amedeo.biolatti@gmail.com>
2023-07-21 12:53:46 +0200

    added node unit test

---
+++ tests/test_node.py
@@ -0,0 +1,23 @@
+import numpy as np
+from tensorflow import keras
+
+import unittest
+
+from keras_neural_trees import node
+
+
+class LatentTreeTestSuit(unittest.TestCase):
+    units: int = 3
+
+    def setUp(self) -> None:
+        keras.backend.clear_session()
+        self.tree = node.NODE(units=self.units)
+        self.x = np.zeros((100, 10))
+
+    def test_output_shape(self):
+        z = self.tree(self.x).numpy()
+        self.assertEqual(z.ndim, 2)
+        self.assertEqual(
+            z.shape,
+            (self.x.shape[0], self.units)
+        )
